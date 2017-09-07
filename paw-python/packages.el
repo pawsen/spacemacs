0e14193eacd14e7222416fc2df95a22ed38af7bb
paw <pawsen@gmail.com>
2017-09-07 22:14:37 +0200

    Use realgud

--- paw-python/packages.el
+++ paw-python/packages.el
@@ -1,6 +1,7 @@
 (setq paw-python-packages
       '(
         (python :location built-in)
+        realgud
         ))
 
 (defun paw-python/post-init-python ()
@@ -20,3 +21,29 @@
     ;; save also with 'kill-this-buffer'
     (add-hook 'kill-buffer-hook 'comint-write-input-ring)
     )))
+
+(defun paw-python/init-realgud()
+  (use-package realgud
+    :defer t
+    :commands (realgud:pdb)
+    :init
+    (progn
+      (spacemacs/set-leader-keys-for-major-mode 'python-mode
+        "dd" 'realgud:pdb
+        "de" 'realgud:cmd-eval-dwim)
+      (advice-add 'realgud-short-key-mode-setup
+                  :before #'spacemacs//short-key-state)
+      (evilified-state-evilify-map realgud:shortkey-mode-map
+        :eval-after-load realgud
+        :mode realgud-short-key-mode
+        :bindings
+        "s" 'realgud:cmd-next
+        "i" 'realgud:cmd-step
+        "b" 'realgud:cmd-break
+        "B" 'realgud:cmd-clear
+        "o" 'realgud:cmd-finish
+        "c" 'realgud:cmd-continue
+        "e" 'realgud:cmd-eval
+        "r" 'realgud:cmd-restart
+        "q" 'realgud:cmd-quit
+        "S" 'realgud-window-cmd-undisturb-src))))
